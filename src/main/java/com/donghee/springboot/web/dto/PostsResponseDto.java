a624924aa39c5a163c6e9dd975e5c9cb9c858e49
Donghee Han <handh0413@gmail.com>
2022-05-14 15:39:05 +0900

    fix: 클래스 이름 수정

--- src/main/java/com/donghee/springboot/web/dto/PostsResponseDto.java
+++ src/main/java/com/donghee/springboot/web/dto/PostsResponseDto.java
@@ -4,14 +4,14 @@ import com.donghee.springboot.domain.posts.Posts;
 import lombok.Getter;
 
 @Getter
-public class PostsResponseEntity {
+public class PostsResponseDto {
 
     private Long id;
     private String title;
     private String content;
     private String author;
 
-    public PostsResponseEntity(Posts entity) {
+    public PostsResponseDto(Posts entity) {
         this.id = entity.getId();
         this.title = entity.getTitle();
         this.content = entity.getContent();
