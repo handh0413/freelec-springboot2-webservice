663fe5ad3d5807d9a1099d6507c11ea9761d18aa
Donghee Han <handh0413@gmail.com>
2022-05-14 15:59:06 +0900

    feat: H2 콘솔 접속을 위한 설정 추가 및 게시글 조회 api 추가

--- src/main/java/com/donghee/springboot/service/PostsService.java
+++ src/main/java/com/donghee/springboot/service/PostsService.java
@@ -26,4 +26,10 @@ public class PostsService {
         posts.update(requestDto.getTitle(), requestDto.getContent());
         return id;
     }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        return new PostsResponseDto(entity);
+    }
 }
--- src/main/java/com/donghee/springboot/web/PostsApiController.java
+++ src/main/java/com/donghee/springboot/web/PostsApiController.java
@@ -1,6 +1,7 @@
 package com.donghee.springboot.web;
 
 import com.donghee.springboot.service.PostsService;
+import com.donghee.springboot.web.dto.PostsResponseDto;
 import com.donghee.springboot.web.dto.PostsSaveRequestDto;
 import com.donghee.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
@@ -20,5 +21,10 @@ public class PostsApiController {
     @PutMapping("/api/v1/posts/{id}")
     public Long update(@PathVariable long id, @RequestBody PostsUpdateRequestDto requestDto) {
         return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
     }
 }
