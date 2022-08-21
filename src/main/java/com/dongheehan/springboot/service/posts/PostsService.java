7da98a52c6a6465418b8730c5b88a22b22f1ea52
Donghee Han <handh0413@gmail.com>
2022-08-21 11:53:21 +0900

    fix: Posts 조회 기능 수정

--- src/main/java/com/dongheehan/springboot/service/posts/PostsService.java
+++ src/main/java/com/dongheehan/springboot/service/posts/PostsService.java
@@ -29,6 +29,7 @@ public class PostsService {
     }
 
     public PostsResponseDto findById(Long id) {
-        return null;
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        return new PostsResponseDto(entity);
     }
 }
