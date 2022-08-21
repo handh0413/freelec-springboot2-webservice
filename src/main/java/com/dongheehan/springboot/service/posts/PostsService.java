e4566fbba2f5b63336e3b4ea8356ee4357f7b066
Donghee Han <handh0413@gmail.com>
2022-08-21 11:39:59 +0900

    feat: Posts 수정 기능 추가

--- src/main/java/com/dongheehan/springboot/service/posts/PostsService.java
+++ src/main/java/com/dongheehan/springboot/service/posts/PostsService.java
@@ -1,9 +1,12 @@
 package com.dongheehan.springboot.service.posts;
 
+import com.dongheehan.springboot.domain.posts.Posts;
 import com.dongheehan.springboot.domain.posts.PostsRepository;
 import com.dongheehan.springboot.web.dto.PostsSaveRequestDto;
+import com.dongheehan.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -11,7 +14,16 @@ public class PostsService {
 
     private final PostsRepository postsRepository;
 
+    @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
     }
 }
