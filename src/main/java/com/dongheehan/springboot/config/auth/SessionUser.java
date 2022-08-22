6bff43a73cf44c357502883083f96aaffa406292
Donghee Han <handh0413@gmail.com>
2022-08-22 21:55:18 +0900

    feat: 구글 로그인 연동하기

---
+++ src/main/java/com/dongheehan/springboot/config/auth/SessionUser.java
@@ -0,0 +1,18 @@
+package com.dongheehan.springboot.config.auth;
+
+import com.dongheehan.springboot.domain.user.User;
+import lombok.Getter;
+
+@Getter
+public class SessionUser {
+
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
