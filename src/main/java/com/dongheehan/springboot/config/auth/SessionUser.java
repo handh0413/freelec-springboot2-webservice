02892cc880790c686afcb44f9813427ff8fc5dd9
Donghee Han <handh0413@gmail.com>
2022-08-23 22:05:01 +0900

    feat: 어노테이션 기반으로 세션 데이터 처리하기

--- src/main/java/com/dongheehan/springboot/config/auth/SessionUser.java
+++ src/main/java/com/dongheehan/springboot/config/auth/SessionUser.java
@@ -3,8 +3,10 @@ package com.dongheehan.springboot.config.auth;
 import com.dongheehan.springboot.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
 
     private String name;
     private String email;
--- src/main/java/com/dongheehan/springboot/web/IndexController.java
+++ src/main/java/com/dongheehan/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.dongheehan.springboot.web;
 
+import com.dongheehan.springboot.config.auth.LoginUser;
 import com.dongheehan.springboot.config.auth.SessionUser;
 import com.dongheehan.springboot.service.posts.PostsService;
 import com.dongheehan.springboot.web.dto.PostsResponseDto;
@@ -19,10 +20,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (user != null) {
             model.addAttribute("userName", user.getName());
